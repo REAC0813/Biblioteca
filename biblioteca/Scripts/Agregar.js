4a5fa06f67711f29af2b76c9c4f9ac32ad241c1a
REAC0813 <85889999+REAC0813@users.noreply.github.com>
2021-06-14 10:17:54 -0500

    Add files via upload

---
+++ biblioteca/Scripts/Agregar.js
@@ -0,0 +1,23 @@
+﻿
+function Agregar() {
+    debugger;
+
+    //$("#ModalAgregar").modal("hide");
+    $.ajax({
+        type: "POST",
+        url: '../Home/_Agregar',
+        traditional: true,
+        success: function (data) {
+            debugger;
+            $('#renderModal').html(data);
+            $("#ModalAgregar").modal('show');
+            $("#ModalAgregar").modal('handleUpdate');
+
+
+
+        },
+
+
+
+    });
+};
